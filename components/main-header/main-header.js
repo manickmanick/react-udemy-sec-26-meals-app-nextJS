5d57bf996059eb79882885a5c1a885facfddfeb1
manickmanick <manickaperumal22@gmail.com>
2026-06-06 22:44:31 +0530

    444 to 448 applied

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,10 +2,13 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "../nav-link";
 
 export default function MainHeader() {
   return (
     <>
+      <MainHeaderBackground />
       <header className={classes.header}>
         <Link href="/" className={classes.logo}>
           <Image src={logoImg} alt="A plate wit food on it" priority></Image>
@@ -14,10 +17,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
